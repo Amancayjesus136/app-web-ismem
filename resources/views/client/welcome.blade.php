21d23799d29c2d66a7ece5d222bedca4f833786b
Amancayjesus136 <amancayjesus136@gmail.com>
2024-09-05 18:32:57 -0500

    deploy test

--- resources/views/client/welcome.blade.php
+++ resources/views/client/welcome.blade.php
@@ -36,8 +36,8 @@
         <nav class="navbar navbar-expand-lg navbar-landing navbar-light bg-custom fixed-top" id="navbar">
             <div class="container">
                 <a class="navbar-brand" href="index.html">
-                    <img src="https://proyecto.ismemvirtual.com/ismem2024/logoismem.png" class="card-logo card-logo-dark" alt="logo dark" height="60" style="border-radius: 30px">
-                    <img src="https://proyecto.ismemvirtual.com/ismem2024/logoismem.png" class="card-logo card-logo-light" alt="logo light" height="60" style="border-radius: 30px">
+                    <img src="{{ asset('assets/img/logo-ismem2024.png') }}" class="card-logo card-logo-dark" alt="logo dark" height="60" style="border-radius: 30px">
+                    <img src="{{ asset('assets/img/logo-ismem2024.png') }}" class="card-logo card-logo-light" alt="logo light" height="60" style="border-radius: 30px">
                 </a>
                 <button class="navbar-toggler py-0 fs-20 text-body" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <i class="mdi mdi-menu"></i>
